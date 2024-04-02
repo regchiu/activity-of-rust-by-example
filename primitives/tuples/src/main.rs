ea6d993584c960aa74149a51578f5070c453ac3e
regchiu <regx1385@gmail.com>
2024-04-02 11:41:36 +0800

    feat(primitives): tuples

--- primitives/tuples/src/main.rs
+++ primitives/tuples/src/main.rs
@@ -18,8 +18,7 @@ struct Matrix(f32, f32, f32, f32);
 
 impl fmt::Display for Matrix {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "( {} {} )\n", self.0, self.1)?;
-        write!(f, "( {} {} )", self.2, self.3)
+        write!(f, "( {} {} )\n( {} {} )", self.0, self.1, self.2, self.3)
     }
 }
 
